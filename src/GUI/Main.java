b2834f1558854bd701b75a9765e52952450d7aef
Sebastian Manterfield <smanterfield@live.co.uk>
2023-02-20 21:58:21 +0000

    Push 15.1) Added console menu to Main class to allow the user to view any screen for testing purposes.

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -1,5 +1,7 @@
 package GUI;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main (String [] args)
@@ -8,9 +10,49 @@ public class Main {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
 
-                //instantiate an object of type screen and run
-                ReviewATestScreen s = new ReviewATestScreen();
-                s.draw(s);
+                System.out.println("Pick a Screen to view: \n1) Welcome Screen\n2)Agent Login Screen\n3)Submit a Test Screen\n4)Tracer Homepage Screen\n5)Review a Test Screen");
+                Scanner in = new Scanner(System.in);
+
+                String sToView = in.nextLine();
+
+               switch (sToView)
+               {
+                   case "1":
+                       WelcomeScreen w = new WelcomeScreen();
+                       w.draw(w);
+                       break;
+                   case "2":
+                       AgentLoginScreen a = new AgentLoginScreen();
+                       a.draw(a);
+                       break;
+                   case "3":
+                       SubmitATestScreen s = new SubmitATestScreen();
+                       s.draw(s);
+                       break;
+                   case "4":
+                       TracerHomepageScreen t = new TracerHomepageScreen();
+                       t.draw(t);
+                       break;
+                   case "5":
+                       ReviewATestScreen r = new ReviewATestScreen();
+                       r.draw(r);
+                       break;
+                   default:
+                       System.out.println("Valid input not entered");
+                       run();
+
+               }
+
+
+
+
+
+
+
+
+
+
+
             }
         });
     }
